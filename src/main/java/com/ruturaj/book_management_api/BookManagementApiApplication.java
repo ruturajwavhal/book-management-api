b127f7e31684950d1c2228eafcd8b71d9ed10c09
ruturajwavhal <ruturajwavhal10@gmail.com>
2025-10-19 19:42:45 +0530

    initial commit

---
+++ src/main/java/com/ruturaj/book_management_api/BookManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.ruturaj.book_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementApiApplication.class, args);
+	}
+
+}
